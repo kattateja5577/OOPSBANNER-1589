f2ab26cae6c34f3657b5a81625db55d548305ecd
Kattateja5577 <kattateja5577@gmail.com>
2026-03-03 13:37:29 +0530

    Added uc1: print oops

--- src/OopsBanner.java
+++ src/OopsBanner.java
@@ -1,6 +1,7 @@
 
 public class OopsBanner{
     public static void main(String[] args){
-        System.out.println("Welcome to oops banner...");
+        // UC1:
+        System.out.println("OOPS");
     }
 }
